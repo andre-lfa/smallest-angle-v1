3aed174567e5428e68960b3e21647a28807d4b5d
Andre LFA <andrelfaareas@gmail.com>
2021-01-08 11:10:11 -0300

    Create function to calculate smallest angle

--- src/controllers/angles.controller.js
+++ src/controllers/angles.controller.js
@@ -3,10 +3,9 @@ const db = require("../config/database");
 // ==> Método responsável por criar um novo 'Calculo de ângulo':
 
 exports.createAngle = async (req, res) => {
-  let { hour, minute } = req.params;
-  minute ? minute : minute = 0;
-  
-  let angle = (60 * hour - 11 * minute) / 2;
+  const { hour, minute } = req.params;
+
+  const angle = calculateSmallestAngle(hour, minute);
 
   const { rows } = await db.query(
     "INSERT INTO calculatedangles (hour, minute, angle) VALUES ($1, $2, $3)",
@@ -17,3 +16,11 @@ exports.createAngle = async (req, res) => {
     angle: angle
   });
 };
+
+function calculateSmallestAngle(hour, minute = 0) {
+  
+  var calculatedAngle = (60 * hour - 11 * minute) / 2;
+  calculatedAngle < 180 ? calculatedAngle : calculatedAngle = calculatedAngle - 180; 
+
+  return calculatedAngle;
+}
